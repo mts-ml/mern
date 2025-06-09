69c028d992533ebc83aad7705cc8215636477022
Mateus <mtsmlima@gmail.com>
2025-06-09 18:43:01 -0300

    First commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+export const Header: React.FC = () => {
+    return (
+        <header className="flex items-center justify-center bg-stone-600 text-white font-semibold h-16">
+            <nav>
+                <h1>HEADER</h1>
+            </nav>
+
+        </header>
+    )
+}
