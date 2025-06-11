60f2e958d8e7514728e4b9d373659a48caabaf94
Mateus <mtsmlima@gmail.com>
2025-06-11 16:35:37 -0300

    Add new configuration

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,21 +1,31 @@
+import 'dotenv/config'
 import express, { NextFunction, Request, Response } from 'express'
+import register from './routes/register'
+import cors from 'cors'
+import mongoose from 'mongoose'
+import { connectDB } from './config/dbConn'
 
 
 const PORT = process.env.PORT || 3500
 const app = express()
 
+// Connect to MongoDB
+connectDB()
+
 app.use(express.json())
 
-// app.use((req: Request, res: Response, next: NextFunction) => {
-    
-// })
+app.use(cors())
 
 app.get('/', (req: Request, res: Response) => {
     res.json({
         message: "Hello World!"
     })
-    console.log('Hello World!')    
 })
 
+app.use('/register', register)
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
+mongoose.connection.once('open', () => {
+    console.log(`Connected to MongoDB`)
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+})
