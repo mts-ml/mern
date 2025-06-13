f711f5bb928e541d7872a34a22bccad4b70984bf
Mateus <mtsmlima@gmail.com>
2025-06-13 14:45:10 -0300

    New file

---
+++ server/src/controller/logoutController.ts
@@ -0,0 +1,49 @@
+import { Request, Response } from "express";
+import { User } from "../model/Users";
+
+
+export async function handleLogout(req: Request, res: Response) {
+    const isProduction = process.env.NODE_ENV === 'production'
+
+    const refreshToken = req.cookies.jwt as string | undefined
+    if (!refreshToken) {
+        res.status(401).json({
+            error: "User unauthorized"
+        })
+        return
+    }
+
+    try {
+        const foundUser = await User.findOne({ refreshToken })
+        if (!foundUser) {
+            res.clearCookie(
+                'jwt',
+                {
+                    httpOnly: true,
+                    secure: isProduction,
+                    sameSite: isProduction ? 'none' : 'lax'
+                }
+            )
+            res.sendStatus(204)
+            return
+        }
+
+        foundUser.refreshToken = ''
+        await foundUser.save()
+
+        res.clearCookie(
+            'jwt',
+            {
+                httpOnly: true,
+                secure: isProduction,
+                sameSite: isProduction ? 'none' : 'lax'
+            }
+        )
+
+        res.sendStatus(204)
+    } catch (error) {
+        console.log(error)
+        res.sendStatus(500)
+        return        
+    }
+}
---
+++ server/src/routes/logout.ts
@@ -0,0 +1,8 @@
+import express from 'express'
+import { handleLogout } from '../controller/logoutController'
+
+
+const router = express.Router()
+router.get('/', handleLogout)
+
+export default router
