dc6cc4e48ec76a4a3feef73ab6edf2cbbc35c7c5
Mateus <mtsmlima@gmail.com>
2025-06-18 12:53:23 -0300

    Comment strict mode

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
@@ -8,11 +8,11 @@ import { AuthProvider } from './Context/AuthProvider.tsx'
 
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  // <StrictMode>
     <AuthProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </AuthProvider>
-  </StrictMode>,
+  // </StrictMode>,
 )
