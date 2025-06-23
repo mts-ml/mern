c2fa3500be11cc72f349bb3ad907716a3f734a7e
Mateus <mtsmlima@gmail.com>
2025-06-23 16:31:59 -0300

    Add new function

--- server/src/utils/utils.ts
+++ server/src/utils/utils.ts
@@ -1,6 +1,12 @@
+import { Request, Response } from "express"
 import { LoginErrors, UserProps } from "../types/userTypes.js"
 
 
 export function addError(errors: LoginErrors, field: keyof UserProps, message: string) {
     errors[field] = [...(errors[field] || []), message]
+}
+
+
+export function getServerTime(req: Request, res: Response) {
+    res.json({ serverTime: Date.now() })
 }
